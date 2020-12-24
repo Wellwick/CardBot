6d9c50245951f785117dd0493880f4d30e63e45e
Wellwick <mbbexley@hotmail.co.uk>
2020-12-24 03:22:28 +0000

    Add submit capabilities

--- stories.py
+++ stories.py
@@ -41,17 +41,22 @@ class Stories(commands.Cog):
     @commands.command()
     async def s(self, ctx, *, args):
         '''Play through an adventure story.
+            %s submit - Get the link to submit a new story
             %s list - Get a list of the available stories
             %s load Story Name - Load in a story
             %s ## - Choose a numbered option in the story
         '''
-        if args[:4] == "list":
+        if args[:4].lower() == "list":
             stories = await sheets.get_stories()
             await ctx.send("Here is a list of the available stories:\n**" + "**, **".join(stories) + "**")
-        elif args[:4] == "load":
+        elif args[:4].lower() == "load":
             story = await sheets.get_story(args[4:].strip())
             self.stories[ctx.author.id] = story
             await self.do_step(ctx)
+        elif args[:6].lower() == "submit":
+            text = "Submit new stories by making a new sheet here: https://docs.google.com/spreadsheets/d/1_fduCenhFnr1Uo1Uu12XinNmGB4N8oMUFMXN2H0IRjc\n"
+            text += "Feel free to use Feline Fury or any other adventure story as an example, and make as many as you like to practice!"
+            await ctx.send(text)
         else:
             try:
                 # This is which index of the options to do
